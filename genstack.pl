e0352470c26b0358a8cc3a22841ff6169ab4adae
Hugothms <hugothms@gmail.com>
2021-03-25 15:05:08 +0100

    message exemple

--- genstack.pl
+++ genstack.pl
@@ -3,7 +3,7 @@
 # Created by tharchen with a ton of <3
 
 # usage: ./genstack stacksize min max
-# exemple: ./genstack 20 0 1000
+# exemple: ./genstack 10 0 1000
 # will creates 10 uniques numbers from 0 to 1000
 # like: 138 527 947 967 811 33 112 526 949 27
 # enjoy :)
